2080926424c020ba35e5fe3e1666c8bb3a1aa6eb
Acespace <alston.clark17@gmail.com>
2013-11-17 18:11:01 -0500

    Alston Clark Update

--- projects/bsearch/RealSeptimalsbseach.py
+++ projects/bsearch/RealSeptimalsbseach.py
@@ -1,4 +1,3 @@
-
 # Alston Clark
 
 def bsearch(list,search):
@@ -34,7 +33,53 @@ def bsearch(list,search):
       return mid
 
 
+# Alston's Update
+
+# Alston Clark
+
+def bsearch(list,search):
+
+
+
+
+  top = len(list)
+
+  bottom = 0
+
+  
+ 
+
+  if (top < search) and (search < bottom):
+    return -1 
+  
+  if (len(list) == 0):
+    return -1 
     
+   
+  while ( bottom <= top):
+    # when bottom == top then you have found the index
+
+
+
+    mid = (top + bottom) / 2
+
+    if (search < list[mid]):
+      top = mid - 1
+
+
+    elif ( search > list[mid]):
+      bottom = mid + 1
+
+
+    
+
+    else:
+      return mid
+
+
+    
+
+
 
 
 
